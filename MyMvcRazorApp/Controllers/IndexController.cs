d32b241b8c8de9eb4be2707400dc94f655089979
wick <wickerdj@gmail.com>
2015-02-26 21:38:11 -0600

    Setup

---
+++ MyMvcRazorApp/Controllers/IndexController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvcRazorApp.Controllers
+{
+    public class IndexController : Controller
+    {
+        public ActionResult Index()
+        {
+			List<IndexModel> newList = new List<IndexModel> ();
+
+			IndexModel newIndexModel = new IndexModel {
+				ID = 1,
+				Description = "First Item",
+				Comments = "This is the first instance of the Model"
+			};
+
+			IndexModel newIndexModel2 = new IndexModel {
+				ID = 2,
+				Description = "Second Item",
+				Comments = "This is the second instance of the Model"
+			};
+
+			newList.Add (newIndexModel);
+			newList.Add (newIndexModel2);
+
+			return View (newList);
+        }
+    }
+}
---
+++ MyMvcRazorApp/Models/IndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyMvcRazorApp
+{
+	public class IndexModel
+	{
+		public IndexModel ()
+		{
+		}
+
+		public int ID { get; set; }
+		public string Description { get; set; }
+		public string Comments { get; set; }
+	}
+}
+
